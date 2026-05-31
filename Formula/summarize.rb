db7be0f7f7f0ec501adf76c7c8af0699669cdf7d
Mike <Mykhailo>
2026-05-31 15:03:53 +0200

    fix: update summarize v0.11.3 hashes (wires agy availability check)

--- Formula/summarize.rb
+++ Formula/summarize.rb
@@ -6,18 +6,18 @@ class Summarize < Formula
   on_macos do
     on_arm do
       url "https://github.com/yetmike/summarize/releases/download/v0.11.3/summarize-macos-arm64-v0.11.3.tar.gz"
-      sha256 "1c4555e35647e22ed3d351e697132997c576fc7dd0b3135fa7e3fe4ec587077b"
+      sha256 "839af2cddb47ec3557915b4ebff778151e6f543a74f6ace9eeae1bd57960ed63"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/yetmike/summarize/releases/download/v0.11.3/summarize-linux-x64-v0.11.3.tar.gz"
-      sha256 "cdf5ff2ec006f40165db8169072c0ed3b97fd2c1c0003a209fc66f752d69d2db"
+      sha256 "ea1ecd70bf1387a970af4954b0d753f6bce0c4383856091a01f2a6b241809d05"
     end
     on_arm do
       url "https://github.com/yetmike/summarize/releases/download/v0.11.3/summarize-linux-arm64-v0.11.3.tar.gz"
-      sha256 "e08641789ef9e70aad0aaa7f6ca5be1f875da521dd8404ac805ba6257d97d2a5"
+      sha256 "9e38c9edce2a27d639f8b06d5182ca580730b49d1cb2016f357c7f59764ef9da"
     end
   end
 
