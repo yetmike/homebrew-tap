55a3d4d3cfe2873b306459c53784aea94e083ee6
Mike <Mykhailo>
2026-05-31 14:53:18 +0200

    chore: bump summarize to v0.11.3 (adds agy CLI provider)

--- Formula/summarize.rb
+++ Formula/summarize.rb
@@ -5,19 +5,19 @@ class Summarize < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/yetmike/summarize/releases/download/v0.11.2/summarize-macos-arm64-v0.11.2.tar.gz"
-      sha256 "7ae2cac0f9fd7e711d0fc67fefe58a059235352c8a06d522e88d55ddf4ee861d"
+      url "https://github.com/yetmike/summarize/releases/download/v0.11.3/summarize-macos-arm64-v0.11.3.tar.gz"
+      sha256 "1c4555e35647e22ed3d351e697132997c576fc7dd0b3135fa7e3fe4ec587077b"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/yetmike/summarize/releases/download/v0.11.2/summarize-linux-x64-v0.11.2.tar.gz"
-      sha256 "4d127466d78052c477f8504524db39e632c0d54777b82cd6be52e9db4253d988"
+      url "https://github.com/yetmike/summarize/releases/download/v0.11.3/summarize-linux-x64-v0.11.3.tar.gz"
+      sha256 "cdf5ff2ec006f40165db8169072c0ed3b97fd2c1c0003a209fc66f752d69d2db"
     end
     on_arm do
-      url "https://github.com/yetmike/summarize/releases/download/v0.11.2/summarize-linux-arm64-v0.11.2.tar.gz"
-      sha256 "59f1eb0bd658495a651ed76152fabef9e26011172c713f5af9829911e6e67705"
+      url "https://github.com/yetmike/summarize/releases/download/v0.11.3/summarize-linux-arm64-v0.11.3.tar.gz"
+      sha256 "e08641789ef9e70aad0aaa7f6ca5be1f875da521dd8404ac805ba6257d97d2a5"
     end
   end
 
